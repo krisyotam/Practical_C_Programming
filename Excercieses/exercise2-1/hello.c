b4fcada2c5842427047155c33f08990c2fc29b69
Kris Yotam <75515498+krisyotam@users.noreply.github.com>
2024-09-26 10:29:42 -0500

    Create hello.c

---
+++ Excercieses/exercise2-1/hello.c
@@ -0,0 +1,27 @@
+/************************************************************
+ *                                                          *
+ *  Filename: hello.c                                       *
+ *  Author:   krisyotam                                     *
+ *  Date:     2024-09-26                                    *
+ *                                                          *
+ *  Description:                                            *
+ *  This program prints hello                               *
+ *                                                          *
+ *                                                          *
+ *  Usage:                                                  *
+ *  Runs the program and the message appears.               *
+ *                                                          *
+ *                                                          *
+ *  Notes:                                                  *
+ *  Demonstration of a simple program                       *
+ *                                                          *
+ ************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+  /*Tell the world hello*/
+  printf("Hello World\n");
+  return(0);
+}
